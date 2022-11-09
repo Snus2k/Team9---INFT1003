86c2c8011dcced4050b102247b4646cc0b314971
Viet Nguyen <nguyenhongthaiviet@hotmail.com>
2022-11-10 00:44:15 +0100

    Lagt til multiplier-mole og correctHits og bomHits variabler

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 const newGameButton = document.getElementById("newGameButton");
+const multiplierLabel = document.getElementById("multiplier");
 
 let hitPosition;
 let result = 0;
@@ -13,6 +14,10 @@ let countDownTimerId = null;
 let moleType;
 let upperCaseLetter;
 let intervalRunning = false;
+let multiplier = false;
+
+let correctHits = 0;
+let wrongHits = 0;
 
 let highscoreArray = [];
 
@@ -90,33 +95,41 @@ function onKeyDown(event) {
         case "greenMole":
           squarePressed.style.backgroundColor = "#43c383";
           hitGreen.play();
-          result++;
+          resultChanger("greenMole");
           score.textContent = result;
+          correctHits++;
           hitPosition = null;
           break;
         case "yellowMole":
           squarePressed.style.backgroundColor = "#43c383";
-          result++;
+          resultChanger("yellowMole");
           score.textContent = result;
           currentTime += 5;
           timeLeft.textContent = currentTime;
+          correctHits++;
           hitPosition = null;
           break;
         case "redMole":
           squarePressed.style.backgroundColor = "#d95959";
-          result--;
+          resultChanger("redMole");
           score.textContent = result;
+          wrongHits++;
           hitPosition = null;
           break;
         case "purpleMole":
           squarePressed.style.backgroundColor = "#43c383";
-          result++;
+          resultChanger("purpleMole");
           score.textContent = result;
+          multiplierFunction();
+          correctHits++;
           hitPosition = null;
           break;
       }
     } else {
-      result--;
+      if (result > 0) {
+        result--;
+      }
+      wrongHits++;
       score.textContent = result;
       // losePoint.play();
       hitPosition = null;
@@ -133,12 +146,25 @@ function countDown() {
 
   if (currentTime == 0) {
     intervalRunning = false;
+    multiplierLabel.style.visibility = "hidden";
     intervalManager();
     checkHighscore();
   }
 }
-//deklarer countDownTimerId, som kjører countDown()-funksjon på spesifikk intervall (i dette tilfellet hvert 1000 ms)
-//let countDownTimerId = setInterval(countDown, 1000);
+
+function intervalManager() {
+  //Kansellerer følgende tidsfunksjoner som kjører på intervaller
+  clearInterval(countDownTimerId);
+  clearInterval(timerID);
+
+  //Hvis intervalRunning = true (når spiller trykker på newGameButton), vil intervallene kjøres
+  if (intervalRunning) {
+    //kjører randomSquare()-funksjon på specifikk tidsintervall
+    timerID = setInterval(randomSquare, 1000);
+    //kjører countDown()-funksjon på spesifikk tidsintervall
+    countDownTimerId = setInterval(countDown, 1000);
+  }
+}
 
 //======================== HIGH SCORE FUNCTIONS
 function compareNumbers(a, b) {
@@ -146,11 +172,17 @@ function compareNumbers(a, b) {
 }
 
 function checkHighscore() {
-  //======== HVIS SCORE UNDER ELLER LIK 0
+  //======== HVIS SCORE UNDER ELLER LIK 0, gi trøste-beskjed
   if (result <= 0) {
-    //Betingelse 2: For lav skåre for å legge i tom array
     noScore.play();
-    alert("Godt forsøk! Øvelse gjør mester. ");
+    alert(
+      "--- RESULTS: Correct Hits = " +
+        correctHits +
+        " Wrong Hits = " +
+        wrongHits +
+        " ---" +
+        " Nice try! There's room for improvement."
+    );
   } else {
     //====== HVIS TOM ARRAY
     if (highscoreArray.length >= 0) {
@@ -160,8 +192,13 @@ function checkHighscore() {
         console.log("Legge skåre i tom array");
 
         let username = prompt(
-          "Bra jobba! Du har oppnådd en high score som er verdig å legge til i topp ti!",
-          "Skriv inn ditt navn her"
+          "--- RESULTS: Correct Hits = " +
+            correctHits +
+            " Wrong Hits = " +
+            wrongHits +
+            " ---" +
+            " GOOD JOB! Your results can be added to TOP TEN HIGH SCORES!",
+          "Please insert your name"
         );
 
         highscoreArray.push({ navn: username, highscore: result });
@@ -174,8 +211,13 @@ function checkHighscore() {
         highscoreArray.forEach((highscoreObject) => {
           if (result > highscoreObject[highscore]) {
             let username = prompt(
-              "Bra jobba! Du har oppnådd en high score som er verdig å legge til i topp ti!",
-              "Skriv inn ditt navn her"
+              "--- RESULTS: Correct Hits = " +
+                correctHits +
+                " Wrong Hits = " +
+                wrongHits +
+                " ---" +
+                " GOOD JOB! Your results can be added to TOP TEN HIGH SCORES!",
+              "Please insert your name"
             );
 
             const index = highscoreArray.findIndex((object) => {
@@ -186,14 +228,26 @@ function checkHighscore() {
 
             highscoreArray.push({ navn: username, highscore: result });
           } else {
-            alert("Godt forsøk! Øvelse gjør mester. ");
+            alert(
+              "--- RESULTS: Correct Hits = " +
+                correctHits +
+                " Wrong Hits = " +
+                wrongHits +
+                " ---" +
+                " GOOD JOB!"
+            );
           }
         });
       } else if (highscoreArray.length < 10) {
         //Betingelse 2: Legge til skåre i array som ikke har 10 elementer ennå
         let username = prompt(
-          "Bra jobba! Du har oppnådd en high score som er verdig å legge til i topp ti!",
-          "Skriv inn ditt navn her"
+          "--- RESULTS: Correct Hits = " +
+            correctHits +
+            " Wrong Hits = " +
+            wrongHits +
+            " ---" +
+            " GOOD JOB! Your results can be added to TOP TEN HIGH SCORES!",
+          "Please insert your name"
         );
 
         highscoreArray.push({ navn: username, highscore: result });
@@ -207,25 +261,52 @@ function checkHighscore() {
 
 newGameButton.addEventListener("click", newGame);
 
+//resette spillet og alle tellervariabler
 function newGame() {
   result = 0;
   score.textContent = result;
   currentTime = 10;
   timeLeft.textContent = currentTime;
+  multiplier = false;
+  multiplierLabel.style.visibility = "hidden";
+  correctHits = 0;
+  wrongHits = 0;
   intervalRunning = true;
   intervalManager();
 }
 
-function intervalManager(interval, time) {
-  //Kansellerer alle tidsfunksjonene som kjører på intervaller
-  clearInterval(countDownTimerId);
-  clearInterval(timerID);
-
-  //Hvis intervalRunning = true (når spiller trykker på newGameButton), vil intervallene kjøres
-  if (intervalRunning) {
-    //kjører randomSquare()-funksjon på specifikk tidsintervall
-    timerID = setInterval(randomSquare, 1000);
-    //kjører countDown()-funksjon på spesifikk tidsintervall
-    countDownTimerId = setInterval(countDown, 1000);
+//endrer result basert på moletype og multiplier
+function resultChanger(moleType) {
+  if (moleType == "redMole") {
+    //Sikre at skåre aldri går under 0, selv med eller uten multiplier
+    if (multiplier && result >= 2) {
+      result -= 2;
+    } else if (multiplier && result == 1) {
+      result--;
+    } else if (result > 0) {
+      result--;
+    }
+  } else {
+    if (multiplier) {
+      result += 2;
+    } else {
+      result++;
+    }
   }
+}
+
+//regulerer multiplier-variabel
+function multiplierFunction() {
+  //activate multiplier
+  multiplier = true;
+  multiplierLabel.style.visibility = "visible";
+  //  multiplierTime = 10;
+  console.log("Multiplier = " + multiplier);
+
+  //after 10 seconds, set multiplier to false
+  setTimeout(() => {
+    multiplier = false;
+    multiplierLabel.style.visibility = "hidden";
+    console.log("Multiplier = " + multiplier);
+  }, 10000);
 }
