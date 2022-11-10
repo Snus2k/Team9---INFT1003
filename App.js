9f12ba1271a0425909784acaecd657169c139bef
Viet Nguyen <nguyenhongthaiviet@hotmail.com>
2022-11-10 17:00:02 +0100

    Lagt til bakgrunsmusikk, fikset highscore-funksjonalitet

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 const newGameButton = document.getElementById("newGameButton");
+const toggleSoundButton = document.getElementById("toggleSoundButton");
 const multiplierLabel = document.getElementById("multiplier");
 
 let hitPosition;
@@ -26,6 +27,22 @@ let madeTheList = new Audio("./Lyder/KomInnPåLista.mp3");
 let noScore = new Audio("./Lyder/KomIkkeInnPåLista.mp3");
 let losePoint = new Audio("./Lyder/MisterEtPoeng.mp3");
 let hitGreen = new Audio("./Lyder/TraffGrønn.mp3");
+
+const backgroundMusic = document.createElement("audio");
+backgroundMusic.setAttribute("src", "./Lyder/BackgroundSong.mp3");
+let toggleSound = true;
+
+//Looping av bakgrunnsmusikk når spillet er igang
+backgroundMusic.addEventListener(
+  "ended",
+  function () {
+    if (intervalRunning && toggleSound) {
+      this.currentTime = 0;
+      this.play();
+    }
+  },
+  false
+);
 
 //======================= 1. RANDOM SQUARE FUNCTION: fjern alle moles og legg til mole i random square ======================
 function randomSquare() {
@@ -74,12 +91,12 @@ function moleAssign(moletypeNummer, randomSquare) {
 
 //======================= 2. KEYBOARD FUNCTIONS ======================
 
-window.addEventListener("keydown", onKeyDown, true);
+window.addEventListener("keypress", onKeyDown, true);
 
 function onKeyDown(event) {
   if (intervalRunning) {
     //konverter Unicode til bokstaver
-    let letter = String.fromCharCode(event.keyCode);
+    let letter = String.fromCharCode(event.which);
 
     upperCaseLetter = letter.toUpperCase();
 
@@ -144,11 +161,19 @@ function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime;
 
+  //Hvis tid er over
   if (currentTime == 0) {
     intervalRunning = false;
     multiplierLabel.style.visibility = "hidden";
+    squares.forEach((square) => {
+      //fjern moleType-class fra alle square-elementer i squares-array
+      square.classList.remove(moleType);
+    });
+
     intervalManager();
     checkHighscore();
+    backgroundMusic.currentTime = 0;
+    backgroundMusic.pause();
   }
 }
 
@@ -172,11 +197,14 @@ function compareNumbers(a, b) {
 }
 
 function checkHighscore() {
-  //======== HVIS SCORE UNDER ELLER LIK 0, gi trøste-beskjed
+  //======== 1.  HVIS SCORE UNDER ELLER LIK 0, gi trøste-beskjed
   if (result <= 0) {
     noScore.play();
+    console.log("Score <= 0, Gi trøstebeskjed");
     alert(
-      "--- RESULTS: Correct Hits = " +
+      "--- RESULTS: Total score = " +
+        result +
+        " Correct Hits = " +
         correctHits +
         " Wrong Hits = " +
         wrongHits +
@@ -184,64 +212,91 @@ function checkHighscore() {
         " Nice try! There's room for improvement."
     );
   } else {
-    //====== HVIS TOM ARRAY
-    if (highscoreArray.length >= 0) {
-      //Betingelse 1: Legge skåre i tom array
-      if (result > 0) {
-        madeTheList.play();
-        console.log("Legge skåre i tom array");
+    //========= 2. HVIS SCORE ER OVER 0:
+    //2.1 HVIS TOM ARRAY
+    if (highscoreArray.length == 0) {
+      //Legge skåre i tom array
+      madeTheList.play();
+      console.log("Legge skåre i tom array");
 
-        let username = prompt(
-          "--- RESULTS: Correct Hits = " +
-            correctHits +
-            " Wrong Hits = " +
-            wrongHits +
-            " ---" +
-            " GOOD JOB! Your results can be added to TOP TEN HIGH SCORES!",
-          "Please insert your name"
-        );
+      let username = prompt(
+        "--- RESULTS: Total score = " +
+          result +
+          " Correct Hits = " +
+          correctHits +
+          " Wrong Hits = " +
+          wrongHits +
+          " ---" +
+          " GOOD JOB! Your results can be added to TOP TEN HIGH SCORES!",
+        "Please insert your name"
+      );
 
-        highscoreArray.push({ navn: username, highscore: result });
-      }
+      highscoreArray.push({ navn: username, highscore: result });
     } else {
-      //======= HVIS IKKE TOM ARRAY
+      //2.2 HVIS IKKE TOM ARRAY
 
-      //Betingelse 1: Legge til skåre i array som allerede har 10 elementer
+      //2.2.1: Hvis highscoreArray for TOPP TI allerede har 10 highscore-elementer
       if (highscoreArray.length >= 10) {
+        //lag midlertidig alternativ tempHighScoreArray for å utføre beregninger
+        const tempHighScoreArray = [];
+
         highscoreArray.forEach((highscoreObject) => {
-          if (result > highscoreObject[highscore]) {
-            let username = prompt(
-              "--- RESULTS: Correct Hits = " +
-                correctHits +
-                " Wrong Hits = " +
-                wrongHits +
-                " ---" +
-                " GOOD JOB! Your results can be added to TOP TEN HIGH SCORES!",
-              "Please insert your name"
-            );
-
-            const index = highscoreArray.findIndex((object) => {
-              return object.highscore === highscoreObject[highscore];
-            });
-
-            highscoreArray.splice(index, 1);
-
-            highscoreArray.push({ navn: username, highscore: result });
-          } else {
-            alert(
-              "--- RESULTS: Correct Hits = " +
-                correctHits +
-                " Wrong Hits = " +
-                wrongHits +
-                " ---" +
-                " GOOD JOB!"
-            );
-          }
+          tempHighScoreArray.push(highscoreObject["highscore"]);
         });
-      } else if (highscoreArray.length < 10) {
-        //Betingelse 2: Legge til skåre i array som ikke har 10 elementer ennå
+
+        //finn laveste highscore i tempHighscoreArray
+        const lowestHighscore = Math.min(...tempHighScoreArray);
+        //fin index til laveste highscore
+        const index = tempHighScoreArray.findIndex((highscore) => {
+          return highscore === lowestHighscore;
+        });
+        console.log(highscoreArray);
+        console.log(tempHighScoreArray);
+        console.log(lowestHighscore);
+
+        //2.2.1.1 Sjekk om high score er bedre enn laveste highscore i highscoreArray, i så fall fjern gammel laveste skåre og legg til ny
+        if (result > lowestHighscore) {
+          console.log(" Legge til skåre i array som allerede har 10 elementer");
+          let username = prompt(
+            "--- RESULTS: Total score = " +
+              result +
+              " Correct Hits = " +
+              correctHits +
+              " Wrong Hits = " +
+              wrongHits +
+              " ---" +
+              " GOOD JOB! Your results can be added to TOP TEN HIGH SCORES!",
+            "Please insert your name"
+          );
+
+          highscoreArray.splice(index, 1);
+
+          highscoreArray.push({ navn: username, highscore: result });
+        } else {
+          //2.2.1.2 Hvis high score ikke er bedre enn laveste highscore  i highscoreArray -> Ingen ny skåre legges til i array
+          console.log(
+            "High score er ikke bedre enn noen av de 10 highscore-elementene i highscoreArray, ingen skåre legges til i array"
+          );
+          alert(
+            "--- RESULTS: Total score = " +
+              result +
+              " Correct Hits = " +
+              correctHits +
+              " Wrong Hits = " +
+              wrongHits +
+              " ---" +
+              " GOOD JOB!"
+          );
+        }
+      }
+
+      //2.2.2: Hvis highscoreArray ennå ikke har 10 elementer, og skåre er over 0 -> Legg til skåre i array
+      else if (highscoreArray.length < 10) {
+        console.log("HighscoreArray har ikke 10 elementer, og skåre er over 0");
         let username = prompt(
-          "--- RESULTS: Correct Hits = " +
+          "--- RESULTS: Total score = " +
+            result +
+            " Correct Hits = " +
             correctHits +
             " Wrong Hits = " +
             wrongHits +
@@ -260,6 +315,19 @@ function checkHighscore() {
 //======================== ANDRE FUNKSJONER
 
 newGameButton.addEventListener("click", newGame);
+toggleSoundButton.addEventListener("click", () => {
+  if (toggleSound) {
+    toggleSoundButton.innerHTML = "Music OFF";
+    backgroundMusic.pause();
+    toggleSound = false;
+  } else {
+    toggleSoundButton.innerHTML = "Music ON";
+    toggleSound = true;
+    if (intervalRunning) {
+      backgroundMusic.play();
+    }
+  }
+});
 
 //resette spillet og alle tellervariabler
 function newGame() {
@@ -273,6 +341,14 @@ function newGame() {
   wrongHits = 0;
   intervalRunning = true;
   intervalManager();
+
+  backgroundMusic.currentTime = 0;
+  backgroundMusic.pause();
+
+  //reset background music
+  if (toggleSound) {
+    backgroundMusic.play();
+  }
 }
 
 //endrer result basert på moletype og multiplier
@@ -281,6 +357,7 @@ function resultChanger(moleType) {
     //Sikre at skåre aldri går under 0, selv med eller uten multiplier
     if (multiplier && result >= 2) {
       result -= 2;
+      //Sikre at skåre ikke går under null når en har result = 1, med multiplier aktivert
     } else if (multiplier && result == 1) {
       result--;
     } else if (result > 0) {
